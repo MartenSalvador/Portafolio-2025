6b3b156cdefe5395284dfcf67e7acd9bbb23664b
Martin Salvador <martinandressalvador@gmail.com>
2025-08-25 15:33:56 +0200

    asegurando key firebase

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCGvfLWgL7JBoZf6TTUU_NGdbhaPvxhzCg",
+  apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "martinsalvadorportafolio.firebaseapp.com",
   projectId: "martinsalvadorportafolio",
   storageBucket: "martinsalvadorportafolio.firebasestorage.app",
