0d318f9019e799080e403954d056456d37655ba8
Martin Salvador <martinandressalvador@gmail.com>
2025-08-25 15:03:18 +0200

    Creando hosting en firebase

---
+++ dataconnect-generated/js/example-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'example',
+  service: 'portafolio',
+  location: 'us-central1'
+};
+
---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCGvfLWgL7JBoZf6TTUU_NGdbhaPvxhzCg",
+  authDomain: "martinsalvadorportafolio.firebaseapp.com",
+  projectId: "martinsalvadorportafolio",
+  storageBucket: "martinsalvadorportafolio.firebasestorage.app",
+  messagingSenderId: "1075068396114",
+  appId: "1:1075068396114:web:589efeecd728f13aa1b58b",
+  measurementId: "G-RWSZLHM26Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
